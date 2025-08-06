6ec097b186f0d4559d6225bee160a1895534ea50
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2025-08-06 13:05:05 +0530

    Finalized

--- app/cloudinary_utils.py
+++ app/cloudinary_utils.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME", "dlovcfdar"),
-    api_key=os.getenv("CLOUDINARY_API_KEY", "535447897778991"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET", "zyBsfx15um1j5Fst3qocTKRkUpk")
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
